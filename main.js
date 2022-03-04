e3a1b5209ba5ee57877d370ab6c9e434c27778e4
Guilherme Torres <53585353+guibbers@users.noreply.github.com>
2022-03-04 01:53:44 -0300

    finish lesson 5

--- main.js
+++ main.js
@@ -6,8 +6,26 @@ const app = new Vue({
     inStock: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
-      { variantId: 2234, variantColor: 'green' },
-      { variantId: 2235, variantColor: 'blue' },
+      {
+        variantId: 2234,
+        variantColor: 'green',
+        variantImage: './assets/vmSocks-green-onWhite.jpg',
+      },
+      {
+        variantId: 2235,
+        variantColor: 'blue',
+        variantImage: './assets/vmSocks-blue-onWhite.jpg',
+      },
     ],
+    cart: 0,
+  },
+
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(image) {
+      this.image = image
+    },
   },
 })
